ed787859c96121ce2c5256f9e628b32089d28a0e
albacarmen <carmenisabelalba@gmail.com>
2024-10-07 17:44:50 -0400

    Famous Quotes

---
+++ Workbook-3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.util.Scanner
+
+public class FamousQuotes {
+    private static int numbers;
+
+    public static void main(String[] args) {
+        String[] quotes ={
+        "I'm literally angry with rage!",
+        "Bite my shiny metal ass",
+        "Shut Up and take my money",
+        "Oh. Your. God",
+        "I don't want to live in this planet anymore",
+        "A headache with pictures",
+        "I have to protect my children from evolution"
+        };
+
+        Scanner scanner = new Scanner(System.in)
+                System.out.println("Select a number between 1 and 7");
+        int number = scanner.nextInt();
+
+        System.out.println("Futurama Quote: " + quotes[numbers-1]);
+        scanner.close();
+    }
+}
