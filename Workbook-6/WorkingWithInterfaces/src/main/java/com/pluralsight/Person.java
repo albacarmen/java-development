1bac80c236e31b764c7e08969ce8b0d65d2c4822
albacarmen <carmenisabelalba@gmail.com>
2024-11-01 11:38:13 -0400

    Implements Comparable

---
+++ Workbook-6/WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class implements Comparable <Person> {
+    String firstName;
+    String lastName;
+    int age;
+
+    public 
+    }
+}
